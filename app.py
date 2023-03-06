0713e482194db350ac645fbe1430475e2eb1df88
stjepan.tadic <stjepan.tadich@gmail.com>
2023-03-06 12:36:07 +0000

    Configuration done (imports, sessions, header responses...)

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+from cs50 import SQL
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem, not cookies
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 library to use SQLite database
+db = SQL("sqlite:///library.db")
+
+# Ensure responses are not cached for privacy reasons
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
