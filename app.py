49501f82367bb094969757d28f82a6c0c26d84ce
stjepan.tadic <stjepan.tadich@gmail.com>
2023-03-12 15:09:46 +0000

    Message flashing for input errors added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from cs50 import SQL
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, flash
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -55,14 +55,16 @@ def login():
     else:
         # Ensure username and password are submitted
         if not request.form.get('username') or not request.form.get('password'):
-            return render_template('login.html', error='Username and password fields are required')
+            flash('Username and password are required fields!')
+            return render_template('login.html')
         
         # Query database for username
         user_name = db.execute('SELECT * FROM staff WHERE username = ?', request.form.get('username'))
 
         # Ensure username exists and password is correct
         if len(user_name) != 1 or not check_password_hash(user_name[0]['hash'], request.form.get('password')):
-            return render_template('login.html', error='Incorrect username and/or password')
+            flash('Incorrect username and/or password!')
+            return render_template('login.html')
         
         # Remember user that has logged in
         session['user_id'] = user_name[0]['staff_id']
@@ -94,29 +96,34 @@ def register():
     else:
         # Check if name provided
         if not request.form.get('name'):
-            return render_template('register.html', error='Please provide name')
+            flash('Name is required field')
+            return render_template('register.html')
 
         # Get username from user input
         user_name = request.form.get('username')
         
         # Check if username provided
         if not user_name:
-            return render_template('register.html', error='Please provide username')
+            flash('Username is required field')
+            return render_template('register.html')
         
         # Check if username already in database
         elif db.execute('SELECT * FROM staff WHERE username = ?', user_name):
-            return render_template('register.html', error='Username already exists')
+            flash('That username already exists')
+            return render_template('register.html')
 
         # Get password from user input
         password = request.form.get('password')
 
         # Check if password provided
         if not password:
-            return render_template('register.html', error='Please provide password')
+            flash('Password is required field')
+            return render_template('register.html')
         
         # Ensure password and confirmation password match
         elif password != request.form.get('confirmation'):
-            return render_template('register.html', error='Password do not match confirmation password')
+            flash('Password and confirmation password do not match')
+            return render_template('register.html')
         
         # Generate hash for password
         hash = generate_password_hash(password)
