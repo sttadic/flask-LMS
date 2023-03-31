9aceb03b4f60418b2fc9cba02873e2100a6f64f9
stjepan.tadic <stjepan.tadich@gmail.com>
2023-03-31 21:35:57 +0100

    Fixed bug in members template where dynamic search didn't work properly. Option value was set to id instead of members_id thus backend couldn't access members_id field when querying database, plus some minor non functional changes to script.js

--- static/script.js
+++ static/script.js
@@ -1,35 +1,35 @@
- // Javascript toggle blur & popup script (books.html/members.html)
- function toggle(){
-    var blur = document.getElementById('blur');
+ // Javascript toggle blur & popup function (books.html/members.html)
+ function toggle() {
+    let blur = document.querySelector('#blur');
     blur.classList.toggle('active');
-    var popup = document.getElementById('popup');
+    let popup = document.querySelector('#popup');
     popup.classList.toggle('active');
-}
+};
 
 
 // Populate popup form fields with corresponding values of a selected book (books.html)
-function editBook(id, title, author, genre, year, stock){
-    document.getElementById("form_id").value = id;
-    document.getElementById("form_title").value = title;
-    document.getElementById("form_author").value = author;
-    document.getElementById("form_genre").value = genre;
-    document.getElementById("form_year").value = year;
-    document.getElementById("form_stock").value = stock;
-}
+function editBook(id, title, author, genre, year, stock) {
+    document.querySelector("#form_id").value = id;
+    document.querySelector("#form_title").value = title;
+    document.querySelector("#form_author").value = author;
+    document.querySelector("#form_genre").value = genre;
+    document.querySelector("#form_year").value = year;
+    document.querySelector("#form_stock").value = stock;
+};
 
 
 // Dynamic search with AJAX and jQuery (books.html)
 function searchBook() {
-    var query = $('#search').val();
-    var field = $('#field').val();
+    let query = $('#search').val();
+    let field = $('#field').val();
     $.get('/books', {'query': query, 'field': field}, function(data) {
-        var table = $('#books tbody');
+        let table = $('#books tbody');
         table.empty();
-        if (query === '') {  // If query empty
-            window.location.reload();  // Reload template
+        if (query === '') {                 // If query empty
+            window.location.reload();       // Reload template
         } else {
             data.forEach(function(book) {
-                var row = $('<tr></tr>');
+                let row = $('<tr></tr>');
                 row.append($('<td></td>').text(book.id));
                 row.append($('<td></td>').text(book.title));
                 row.append($('<td></td>').text(book.author));
@@ -39,38 +39,40 @@ function searchBook() {
                 
                 form = $('<form></form>').attr('action', '/books').attr('method', 'POST').on('click', function(e) {
                     if (!confirm('Completely remove \'' + book.title + '\' by ' + book.author + ' (book ID: ' + book.id + ') and all of its stock from library database?')) {
-                        e.preventDefault();  // Prevent form submission if user cancels
-                    }
+                        // Prevent form submission (default behavior) if user cancels
+                        e.preventDefault();
+                    };
                 });
-                input = $('<input>').attr("name", 'id').attr('value', book.id).hide()
+                input = $('<input>').attr("name", 'id').attr('value', book.id).hide();
                 button = $('<button></button>').text('Remove').addClass('button').attr('name', 'button').attr('value','remove');
-
-                form.append(input).append(button);  // Input & button inside form element
+                // Input & button inside form element
+                form.append(input).append(button);
                 
                 row.append($('<td></td>').append(form));
+                // Toggle and editBook functions appended to a table row along with Edit button and its class
                 row.append($('<td></td>').append($('<button></button>').text('Edit').addClass('button btn-edit').click(function() {
                     toggle();
                     editBook(book.id, book.title, book.author, book.genre, book.year, book.stock);
-                })));   // Toggle and editForm functions appended along with Edit button class
+                })));
                 table.append(row);
             });
-        }
+        };
     });
-}
+};
 
 
 // Dynamic search with AJAX and jQuery (catalogue.html)
 function searchCatalogue() {
-    var query = $('#search').val();
-    var field = $('#field').val();
+    let query = $('#search').val();
+    let field = $('#field').val();
     $.get('/books', {'query': query, 'field': field}, function(data) {
-        var table = $('#books tbody');
+        let table = $('#books tbody');
         table.empty();
-        if (query === '') {  // if query empty
-            window.location.reload();  // reload template
+        if (query === '') {                 // if query empty
+            window.location.reload();       // reload template
         } else {
             data.forEach(function(book) {
-                var row = $('<tr></tr>');
+                let row = $('<tr></tr>');
                 row.append($('<td></td>').text(book.id));
                 row.append($('<td></td>').text(book.title));
                 row.append($('<td></td>').text(book.author));
@@ -80,33 +82,33 @@ function searchCatalogue() {
                 row.append($('<td></td>').text(book.availability));
                 table.append(row);
             });
-        }
+        };
     });
-}
+};
 
 
 // Populate popup form fields with corresponding values of a selected member (members.html)
-function editMember(member_id, name, email, address, phone){
-    document.getElementById("form_id").value = member_id;
-    document.getElementById("form_name").value = name;
-    document.getElementById("form_email").value = email;
-    document.getElementById("form_address").value = address;
-    document.getElementById("form_phone").value = phone;
-}
+function editMember(member_id, name, email, address, phone) {
+    document.querySelector("#form_id").value = member_id;
+    document.querySelector("#form_name").value = name;
+    document.querySelector("#form_email").value = email;
+    document.querySelector("#form_address").value = address;
+    document.querySelector("#form_phone").value = phone;
+};
 
 
 // Dynamic search with AJAX and jQuery (members.html)
 function searchMember() {
-    var query = $('#search').val(); 
-    var field = $('#field').val();
+    let query = $('#search').val(); 
+    let field = $('#field').val();
     $.get('/members', {'query': query, 'field': field}, function(data) {
-        var table = $('#members tbody');
+        let table = $('#members tbody');
         table.empty();
-        if (query === '') {  // If query empty
-            window.location.reload();  // Reload template
+        if (query === '') {                 // If query empty
+            window.location.reload();       // Reload template
         } else {
             data.forEach(function(member) {
-                var row = $('<tr></tr>');
+                let row = $('<tr></tr>');
                 row.append($('<td></td>').text(member.member_id));
                 row.append($('<td></td>').text(member.name));
                 row.append($('<td></td>').text(member.email));
@@ -115,21 +117,23 @@ function searchMember() {
                 
                 form = $('<form></form>').attr('action', '/members').attr('method', 'POST').on('click', function(e) {
                     if (!confirm("Completely remove member " + member.name + " (member ID: " + member.member_id + ") and all their details from library database?")) {
-                        e.preventDefault();  // Prevent form submission if user cancels
-                    }
+                        // Prevent form submission if user cancels
+                        e.preventDefault();
+                    };
                 });
-                input = $('<input>').attr("name", 'id').attr('value', member.member_id).hide()
+                input = $('<input>').attr("name", 'id').attr('value', member.member_id).hide();
                 button = $('<button></button>').text('Remove').addClass('button').attr('name', 'button').attr('value','remove');
-
-                form.append(input).append(button);  // Input & button inside form element
+                // Input & button inside form element
+                form.append(input).append(button);
                 
                 row.append($('<td></td>').append(form));
+                // Toggle and editMember functions appended along with Edit button and its class
                 row.append($('<td></td>').append($('<button></button>').text('Edit').addClass('button btn-edit').click(function() {
                     toggle();
                     editMember(member.member_id, member.name, member.email, member.address, member.phone);
-                })));   // Toggle and editForm functions appended along with Edit button class
+                })));
                 table.append(row);
             });
-        }
+        };
     });
-}
+};
